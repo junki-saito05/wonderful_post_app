74e5949a9ed89f39f7eb3e59e0d71e9987b75170
齊藤洵紀 <Junki.ggg@gmail.com>
2023-05-14 22:32:17 +0900

    create_tagの修正

--- db/migrate/20230514041201_create_tag_articles.rb
+++ db/migrate/20230514041201_create_tag_articles.rb
@@ -1,7 +1,7 @@
 class CreateTagArticles < ActiveRecord::Migration[6.1]
   def change
     create_table :tag_articles do |t|
-      t.refernces :tag, foreign_key: true
+      t.references :tag, foreign_key: true
       t.references :article, foreign_key: true
 
       t.timestamps
